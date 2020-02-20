10d08483906cbbddbbfd822c8fe68fd0db784d12
SquidThePrinter <squidtheprinter@gmail.com>
2020-02-20 15:36:33 -0500

    Readability & Prep for RGAPI Calls

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import discord
 import logging
 import json
+import requests
+import sys
 
 from pathlib import Path
 from datetime import datetime
 from discord.ext import commands
+
+def switch_platform(arg):
+    switch = {
+        'br': 'br1',
+        'eun': 'eun1',
+        'euw': 'euw1',
+        'jp': 'jp1',
+        'kr': 'kr',
+        'lan': 'la1',
+        'las': 'la2',
+        'na': 'na1',
+        'oce': 'oc1',
+        'tr': 'tr1',
+        'ru': 'ru'
+    }
+    return switch.get(arg, '-1')
 
 # TODO: Change debug to be a launch option instead of program variable
 debug = True
@@ -38,6 +56,13 @@ logger.addHandler(loghandler)
 Start Bot
 """
 
+rgkey = settings['riot-api-key']
+
+host = switch_platform(settings['region'])
+if host == '-1':
+    print("Region in config is invald, must be one of the folowing: {'br', 'eun', 'euw', 'jp', 'kr', 'lan', 'las', 'na', 'oce', 'tr', 'ru'}")
+    sys.exit(1)
+
 bot = commands.Bot(command_prefix=settings['prefix'])
 
 #TODO: Put champ quotes in a file and random pull
@@ -48,12 +73,18 @@ async def on_ready():
 #A test command to make sure that the bot is hooking into the Discord API properly
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f':ping_pong: Pong! `{round(bot.latency*1000, 2)} ms`')
+    await ctx.send(f":ping_pong: Pong! `{round(bot.latency*1000, 2)} ms`")
 
 #A test command to make sure the bot is handling arguments properly in the current context
 @bot.command()
-async def echo(ctx, arg):
-    await ctx.send(arg)
+async def echo(ctx, *, args):
+    await ctx.send(args)
+
+#Lookup basic summoner info
+@bot.command()
+async def summoner(ctx):
+    await ctx.send("Not yet implemented")
+
 
 #If the debug flag is enabled log messages to console to ensure the bot is connected properly
 @bot.event
